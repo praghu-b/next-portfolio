23f0d15eff6529b0ca862d4a8158e209f72a2784
praghu-b <prakashbalan555@gmail.com>
2025-10-20 23:15:04 +0530

    feat(ui): initiated cta on about section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -3,9 +3,18 @@ import {motion, useScroll, useTransform} from "framer-motion";
 export default function About() {
     return (
         <section className="w-full min-h-screen">
-            <div className="max-w-7xl mx-auto flex items-center justify-between">
-                <div className="flex-1"></div>
-                <div className="flex-1"></div>
+            <div className="max-w-7xl mx-auto flex items-center bg-purple-500 justify-between text-white md:py-10 mt-20">
+                <div className="flex-2">
+                    <p className="ml-10 text-9xl font-bold font-climate">Me ? <br />I'm Prakash !</p>
+                </div>
+                <div className="flex-1 space-y-10">
+                    <p className="text-xl font-semibold bg-purple-700 shadow shadow-black/50 border-white px-5 py-5">A Full-stack Developer and a Student on his Final Year Computer Science major with 20+ months of internship experience under two firms.</p>
+                    <ul className="flex items-center justify-between">
+                        <li className="font-bold"><span className="text-5xl">20+</span><br />Experience</li>
+                        <li className="font-bold"><span className="text-5xl">10+</span><br />Live Projects</li>
+                        <li className="font-bold"><span className="text-5xl">20+</span><br />Experience</li>
+                    </ul>
+                </div>
             </div>
         </section>
     )
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,15 +3,32 @@ import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { Home, BookUser, FolderCode, PhoneForwarded, Linkedin, Mail, Github, Instagram } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Navbar() {
+    const [hidden, setHidden] = useState(false);
+    const lastScroll = useRef(0);
     const navLinks = [{ label: "Home", icon: Home }, { label: "About", icon: BookUser }, { label: "Projects", icon: FolderCode }, { label: "Contact", icon: PhoneForwarded }];
     const socialLinks = [{ label: "LinkedIn", icon: Linkedin, color: 'bg-blue-700', link: 'https://linkedin.com/in/prakash-balamurugan', dur: 0.2 }, { label: "Email", icon: Mail, color: 'bg-yellow-600', link: 'mailto:prakashbalan555@gmail.com', dur: 0.4 }, { label: "GitHub", icon: Github, color: 'bg-neutral-700', link: 'https://github.com/praghu-b', dur: 0.6 }, { label: "Instagram", icon: Instagram, color: 'bg-gradient-to-tr from-yellow-600 via-pink-600 to-purple-700', link: 'https://instagram.com/praghu._b', dur: 0.8 }]
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const current = window.scrollY;
+            current > lastScroll.current ? setHidden(true) : setHidden(false);
+            lastScroll.current = current;
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [lastScroll])
 
     return (
         <div className="w-full">
             <motion.nav
                 className="max-w-7xl mx-auto fixed top-8 left-10 right-10 flex items-center justify-start z-10"
+                initial={{ y: 0 }}
+                animate={{ y: hidden ? -100 : 0 }}
+                transition={{ duration: 0.3, ease: 'easeOut' }}
             >
                 <motion.div className="flex-1">
                     <span className="text-5xl font-bold font-climate">Pb</span>
