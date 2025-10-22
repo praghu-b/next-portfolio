7a250844bdd983efa3745c9676ed0b7f7ad0dd22
praghu-b <prakashbalan555@gmail.com>
2025-10-22 22:11:25 +0530

    wip(ui): building about section, eodc

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,18 +1,19 @@
-import {motion, useScroll, useTransform} from "framer-motion";
 
 export default function About() {
+
     return (
         <section className="w-full min-h-screen">
-            <div className="max-w-7xl mx-auto flex items-center bg-purple-500 justify-between text-white md:py-10 mt-20">
-                <div className="flex-2">
-                    <p className="ml-10 text-9xl font-bold font-climate">Me ? <br />I'm Prakash !</p>
+            <div className="max-w-7xl mx-auto flex items-center justify-between mt-20">
+                <div className="flex-1 h-full bg-purple-500 md:p-10 text-white font-bold font-climate">
+                    <p className="text-9xl">Me ?</p>
+                    <p className="text-8xl">I'm Prakash !</p>
                 </div>
-                <div className="flex-1 space-y-10">
-                    <p className="text-xl font-semibold bg-purple-700 shadow shadow-black/50 border-white px-5 py-5">A Full-stack Developer and a Student on his Final Year Computer Science major with 20+ months of internship experience under two firms.</p>
-                    <ul className="flex items-center justify-between">
-                        <li className="font-bold"><span className="text-5xl">20+</span><br />Experience</li>
-                        <li className="font-bold"><span className="text-5xl">10+</span><br />Live Projects</li>
-                        <li className="font-bold"><span className="text-5xl">20+</span><br />Experience</li>
+                <div className="flex-1 md:ml-5 md:px-10 py-5 space-y-5">
+                    <p className="text-3xl font-bold mb-5">About Me</p>
+                    <ul className="space-y-5 text-xl">
+                        <li>I'm a Full-stack Developer as well as a Computer Science final year Student with 20+ months of internship experience under two firms. </li>
+                        <li>I specialize in Frontend(React.js, Next.js) and Backend(Express.js, Django) development and currently exploring Agentic AI(LangGraph, LangChain) and Data Engineering as well. </li>
+                        <li>I've 20+ months of internship experience in two smaller firms in coimbatore.</li>
                     </ul>
                 </div>
             </div>
