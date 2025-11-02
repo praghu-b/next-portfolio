f7daaa400689aaa96475db6ec65a10fcf47e79aa
praghu-b <prakashbalan555@gmail.com>
2025-11-02 13:53:11 +0530

    style(hero): redesigned hero section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,75 +1,69 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
-import Prakash from '../../../public/Me/prakash.png';
-import { Code, GraduationCap, HeartHandshake } from 'lucide-react';
+import Prakash from '../../../public/Me/prakash-full.jpg';
+import { Code2, Download } from 'lucide-react';
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { MdMail } from 'react-icons/md';
+import { Button } from '../common/Button';
 
 export default function HeroSection() {
+    const socialLinks = [{ label: "LinkedIn", icon: FaLinkedin, link: 'https://linkedin.com/in/prakash-balamurugan' }, { label: "Email", icon: MdMail, link: 'mailto:prakashbalan555@gmail.com' }, { label: "GitHub", icon: FaGithub, link: 'https://github.com/praghu-b' }, { label: "Instagram", icon: FaInstagram, link: 'https://instagram.com/praghu._b' }]
+
     return (
-        <section className="relative min-h-screen w-full flex items-center justify-center">
-            <div className='absolute w-full md:max-w-7xl flex flex-col items-center justify-between md:px-10 space-y-20 md:mt-50'>
-                <div className='w-full flex font-fancy items-center justify-center text-center'>
-                    <motion.div
-                        className="flex-1 text-8xl"
-                        initial={{ x: "-100vw" }}
-                        animate={{ x: 0 }}
-                        transition={{ delay: 0.2, duration: 0.5, ease: "easeOut" }}
-                    >
-                        Hello,
-                    </motion.div>
-                    <motion.div
-                        className="flex-1 text-8xl"
-                        initial={{ x: "100vw" }}
-                        animate={{ x: 0 }}
-                        transition={{ delay: 0.2, duration: 0.5, ease: "easeOut" }}
-                    >
-                        there!
-                    </motion.div>
-                </div>
-                <div className='w-full flex items-center justify-between'>
-                    <motion.div
-                        className='bg-accent text-foreground flex items-center gap-5 px-4 py-2 rounded-full font-semibold'
-                        initial={{ x: "-100vw" }}
-                        animate={{ x: 0 }}
-                        transition={{ delay: 0.4, duration: 0.5, ease: "easeOut" }}
-                    >
-                        <div className="w-2 h-2 bg-orange-500 rounded-full shadow-[0_0px_5px_5px_rgba(255,105,0,0.5)]"></div>
-                        <span className='text-sm'>Available To Collaborate</span>
-                    </motion.div>
-                    <motion.div
-                        className='backdrop-blur-lg bg-muted border border-muted rounded-2xl px-4 py-2 shadow'
-                        initial={{ x: "100vw" }}
-                        animate={{ x: 0 }}
-                        transition={{ delay: 0.4, duration: 0.5, ease: "easeOut" }}
-                    >
-                        <h1 className='font-semibold'>Specialized in Full-stack <br />Development and API <br />Integration</h1>
-                    </motion.div>
-                </div>
-                <div className="relative w-full flex items-center justify-between">
-                    <motion.h2 className='text-9xl z-10 text-start font-climate font-semibold'
-                        initial={{ x: "-100vw" }}
-                        animate={{ x: 0 }}
-                        transition={{ delay: 0.6, duration: 0.5, ease: "easeOut" }}
-                    >
-                        I AM <br /> PRAKASH
-                    </motion.h2>
-                    <motion.h3 className='text-4xl z-10 text-start font-climate font-semibold'
-                        initial={{ x: "100vw" }}
-                        animate={{ x: 0 }}
-                        transition={{ delay: 0.6, duration: 0.5, ease: "easeOut" }}
-                    >
-                        FULLSTACK <br /> DEVELOPER <br /> AND STUDENT
-                    </motion.h3>
+        <section className="relative min-h-screen w-full flex flex-col items-center justify-center space-y-10">
+            <div className="max-w-7xl mx-auto flex items-center">
+                <motion.div
+                    className='relative flex-2 w-full flex justify-center'
+                    initial={{ y: "50vh", opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.5, ease: "easeOut" }}
+                >
+                    <div className='overflow-hidden rounded-full border-10 border-white shadow-2xl shadow-black/70'>
+                        <Image src={Prakash} alt='Image of Prakash' className='w-[400px] h-[400px] object-cover object-[30%_70%]' />
+                    </div>
+                    <p className='absolute top-15 left-5 bg-primary text-white py-2 px-4 rounded-full font-semibold text-xl'>Hello! I'm Prakash</p>
+                </motion.div>
+                <div className='flex-3 space-y-8'>
+                    <h3 className='text-6xl font-bold md:w-7/8 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent'>Building web apps that think, automate, and perform.</h3>
+                    <h6 className='text-lg text-accent md:w-4/5'>I build scalable web apps, intelligent systems, and products that blend creativity with automation. Passionate about turning complex ideas into real-world digital experiences.</h6>
+                    <div className="flex items-center space-x-5">
+                        <Button label={"Download CV"} variant='primary' icon={Download} onClick={() => window.open("/docs/Prakash_Resume.pdf")} />
+                        <Button label={"View Projects"} variant='outline' icon={Code2} href='#projects' />
+                    </div>
                 </div>
             </div>
-            <motion.div
-                className='absolute w-full flex items-center justify-center'
-                initial={{ y: "200vh" }}
-                animate={{ y: 0 }}
-                transition={{ duration: 0.5, ease: "easeOut" }}
-            >
-                <Image src={Prakash} alt='Image of Prakash' className='w-200 bg-gradient-to-b from-0% via-white/0 to-transparent' />
-            </motion.div>
-        </section>
+            <div className='absolute bottom-10'>
+                <ul className='bg-primary py-2 px-8 rounded-full flex items-center gap-8'>
+                    {socialLinks.map(({ label, link, icon: Icon }) => (
+                        <motion.li
+                            key={label}
+                            whileHover="hover"
+                            initial="initial"
+                            variants={{
+                                hover: {},
+                                initial: {}
+                            }}
+                            className="flex items-center gap-2"
+                        >
+                            <Link href={link} className="flex items-center">
+                                <Icon className="w-8 h-8 text-white" />
+                                <motion.p
+                                    variants={{
+                                        hover: { opacity: 1, width: "auto", marginLeft: 8 },
+                                        initial: { opacity: 0, width: 0, marginLeft: 0 }
+                                    }}
+                                    transition={{ duration: 0.3 }}
+                                    className="text-white font-medium overflow-hidden"
+                                >
+                                    {label}
+                                </motion.p>
+                            </Link>
+                        </motion.li>
+                    ))}
+                </ul>
+
+            </div>
+        </section >
     )
 }
