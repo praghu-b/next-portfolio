b593de0f3faa3b6d9391f48a2862ab61d1775fb9
praghu-b <prakashbalan555@gmail.com>
2025-11-06 19:34:04 +0530

    feat(ui): add project card to project section

--- src/components/sections/Project.tsx
+++ src/components/sections/Project.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Prakash from "../../../public/me/ooty-landscape.jpeg"
+import { Button } from "../common/Button";
 
 export default function ProjectSection() {
 
     return (
         <section className="relative min-h-screen w-full">
-            <div className="max-w-7xl mx-auto">
+            <div className="max-w-7xl mx-auto space-y-24">
                 <div className="flex items-center justify-between">
                     <div className="space-y-4">
                         <h1 className="text-4xl font-bold">Things I've Built!</h1>
@@ -19,8 +21,24 @@ export default function ProjectSection() {
                         #CodeInAction
                     </motion.p>
                 </div>
-                <div className="">
-                    
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                    <div className="relative">
+                        <div className="absolute -top-5 w-3/5 h-8 bg-primary rounded-t-xl"></div>
+                        <div className="flex flex-col shadow-lg rounded-2xl overflow-hidden">
+                            <div className="flex-3 h-80 pl-3 pr-1 pt-3 bg-primary">
+                                <Image src={Prakash} alt="Prakash" />
+                            </div>
+                            <div className="flex-1 flex items-center justify-between p-4 bg-muted">
+                                <div className="flex-3 space-y-2">
+                                    <p className="text-xl text-primary font-bold">Project Title</p>
+                                    <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, quod.</p>
+                                </div>
+                                <div className="flex-1 flex justify-center">
+                                    <Button label={'Open'} variant="primary"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
