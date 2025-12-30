49bcfb300140904ac1f0678f16942ded67a00915
praghu-b <prakashbalan555@gmail.com>
2025-12-30 21:19:58 +0530

    refactor(api): move fetch url to env variable

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -37,7 +37,7 @@ export default function ContactSection() {
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
-        const res = await fetch("/api/contact", {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/contact`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formData),
